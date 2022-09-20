c564e2d76aff9f4eb1486bdb62c0b1e3bf528eca
HenriqueFranca <henriqsf@gmail.com>
2022-09-20 12:11:01 -0300

    Variaveis e seletores

---
+++ index.js
@@ -0,0 +1,10 @@
+var nome = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputNamePassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+
