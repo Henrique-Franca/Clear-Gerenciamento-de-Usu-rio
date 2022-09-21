de17fa9b4d203a9f1f0b4b54fe74c470e01711b0
HenriqueFranca <henriqsf@gmail.com>
2022-09-21 11:38:17 -0300

    File Reader

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -19,11 +19,50 @@ class UserController{
     
             event.preventDefault();
     
-            this.addLine(this.getVelue());
+            let values = this.getVelue();
 
+            
+
+            this.getPhoto((content)=>{
+
+                values.photo = content;
+                this.addLine(values);
+            });
         });
 
     }
+
+
+    //metodo para achar o caminho da foto
+    getPhoto(callback){
+        
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item=>{
+
+             if (item.name === 'photo') {
+                
+               return item;
+
+             } 
+
+        });
+       
+        let file = elements[0].files[0]
+
+        fileReader.onload = () =>{
+
+            
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
+
+    }
+
+
+
 
     //metodo para pegar os valores
     getVelue(){
@@ -59,7 +98,7 @@ class UserController{
         
         this.tableEl.innerHTML = `
         <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
         <td>${dataUser.admin}</td>
