eaa7e2fc4bd6b6c50b2338101083bb175da165c8
HenriqueFranca <henriqsf@gmail.com>
2022-09-21 09:46:58 -0300

    Classes e MVC

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user ={};
 
+
+//funçao para definir valores nas tabelas
 function addLine(dataUser){
 
-    var tr = document.createElement("tr");
-    
+    console.log(dataUser);
 
-    tr.innerHTML = `<tr>
+
+    document.getElementById("table-users").innerHTML = `
+    <tr>
     <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
     <td>${dataUser.name}</td>
     <td>${dataUser.email}</td>
@@ -16,10 +19,9 @@ function addLine(dataUser){
       <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
       <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
     </td>
-  </tr>`;
-
-  document.getElementById("table-users").appendChild(tr);
-
+  </tr>
+  `
+  ;
 }
 
 
@@ -44,7 +46,12 @@ document.getElementById("form-user-create").addEventListener("submit",function(e
     
     
     });
-    addLine(user);
+
+    // objeto referente a classe User
+    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+    
+    
+    addLine(objectUser);
 
 });
 
