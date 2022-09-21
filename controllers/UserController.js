bf1a1f7860ad6a27aa6beb9ccfd2e5d872e2d97b
HenriqueFranca <henriqsf@gmail.com>
2022-09-21 12:28:12 -0300

    Operador Ternário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -19,20 +19,32 @@ class UserController{
     
             let values = this.getVelue();
 
-            this.getPhoto((content)=>{
-                //
-                values.photo = content;
-                this.addLine(values);
-            });
+            this.getPhoto().then(
+                (content)=>{
+
+                    values.photo = content;
+                    this.addLine(values);
+
+                },
+                (e)=>{
+
+                    console.error(e);
+
+                }
+            );
+
+            
         });
 
     }
 
 
     //metodo para achar o caminho da foto
-    getPhoto(callback){
+    getPhoto(){
         
-        let fileReader = new FileReader();
+        return new Promise((resolve, reject)=>{
+
+            let fileReader = new FileReader();
 
         let elements = [...this.formEl.elements].filter(item=>{
 
@@ -49,11 +61,25 @@ class UserController{
         fileReader.onload = () =>{
 
             
-            callback(fileReader.result);
+            resolve(fileReader.result);
 
         };
 
-        fileReader.readAsDataURL(file);
+        fileReader.onerror = (e)=>{
+
+            reject(e);
+
+        };
+
+        if(file){
+            fileReader.readAsDataURL(file);
+        } else{
+            resolve('dist/img/boxed-bg.jpg');
+        }
+
+        });
+
+        
 
     }
 
@@ -72,7 +98,11 @@ class UserController{
                     user[field.name] = field.value;
                 }
         
-            }else {
+            }else if(field.name == 'admin'){
+
+                user[field.name] = field.checked;
+
+            } else {
                 user[field.name] = field.value;
             }
         
@@ -91,21 +121,22 @@ class UserController{
     //funçao para definir valores nas tabelas
     addLine(dataUser){
 
-        
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = ` 
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
         <td>${dataUser.birth}</td>
         <td>
         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-        </tr>
+        </td>    
         `
         ;
+        
+        this.tableEl.appendChild(tr);  
     }
 
 }
