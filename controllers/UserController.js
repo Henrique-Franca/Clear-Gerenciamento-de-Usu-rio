c3110d04f770c57cc7c247a142804305181334ee
HenriqueFranca <henriqsf@gmail.com>
2022-09-22 10:03:37 -0300

    Usando dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -23,6 +23,8 @@ class UserController{
     
             let values = this.getVelue();
 
+            if(!values) return false;
+
             this.getPhoto().then(
                 (content)=>{
 
@@ -102,7 +104,7 @@ class UserController{
 
         [...this.formEl.elements].forEach(function(field, index){
 
-            if(['name','email','passowrd'].indexOf(field.name)> -1 && !field.value ){
+            if(['name','email','password'].indexOf(field.name)> -1 && !field.value ){
 
                 field.parentElement.classList.add('has-error');
                 isValid = false;
@@ -145,6 +147,8 @@ class UserController{
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = ` 
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -159,6 +163,30 @@ class UserController{
         ;
         
         this.tableEl.appendChild(tr);  
+
+        this.updateCount();
+    }
+
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr =>{
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmin++;
+
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
     }
 
 }
